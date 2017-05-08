f9ce06d590bb60854f5404581ccf48033f554620
Erhan Kartaltepe <erhan.kartaltepe@translationalsoftware.com>
2017-05-08 15:31:23 -0700

    Removed unused usings.

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -1,10 +1,6 @@
 ﻿using SheepAspect.Framework;
 using SheepAspect.Runtime;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Driver
 {
